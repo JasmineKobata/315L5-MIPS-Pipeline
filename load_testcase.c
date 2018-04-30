07537751d78ca1a956495df44ee760605e082d17
SkyborneVeggies <jasmineorjasnice@gmail.com>
2018-04-30 10:58:23 -0700

    Add files via upload

--- load_testcase.c
+++ load_testcase.c
@@ -8,7 +8,11 @@
 
 
 #include <stdio.h>
-#include <stdlib.h>
+#include imulate the MIPS instruction set and execute the
+countbits_benchmark_nop program. Display the statistics values requested in the Detailed
+Description. In addition to the code needed to implement the lab, include the resulting
+statistics obtained by running the benchmark. (As usual, please include the source code as
+additional files or within a zipstdlib.h>
 #include <string.h>
 
 #include "mips_asm_header.h"
@@ -18,6 +22,28 @@ typedef unsigned int MIPS, *MIPS_PTR;
 
 MB_HDR mb_hdr;		/* Header area */
 MIPS mem[1024];		/* Room for 4K bytes */
+int haltflag;
+int PCCOUNT;
+
+int if(int pcc) {
+	return pcc + 4;
+}
+
+void id(int *rfil) {
+
+}
+
+void ex() {
+
+}
+
+void mem() {
+
+}
+
+void wb() {
+
+}
 
 main(int argc, char *argv[])
 {
@@ -28,7 +54,11 @@ main(int argc, char *argv[])
 
 /* format the MIPS Binary header */
 
-	fd = fopen(argv[1], "rb");
+	fd = fopen(argv[1], "rb"e to simulate the MIPS instruction set and execute the
+countbits_benchmark_nop program. Display the statistics values requested in the Detailed
+Description. In addition to the code needed to implement the lab, include the resulting
+statistics obtained by running the benchmark. (As usual, please include the source code as
+additional files or within a zip);
 	if (fd == NULL) { printf("\nCouldn't load test case - quitting.\n"); exit(99); }
 
 	memp = 0;		/* This is the memory pointer, a byte offset */
@@ -57,6 +87,7 @@ main(int argc, char *argv[])
 
 	int opcode = 0;
 	int rs, rt, rd, shamt, imm, signext;
+	int REGFILE[32];
 	char *REG[32] = {"$zero", "$at", "$v0", "$v1", "$t0", "$t1", "$t2", "$t3", "$t4",
 		"$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6",
 		"$s7", "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"};
@@ -68,59 +99,66 @@ main(int argc, char *argv[])
 		"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
 		"", "", "add", "addu", "sub", "subu", "and", "or", "xor", "nor", "", "",
 		"slt", "sltu"};
-	for (i = 0; i<memp; i+=4)	/* i contains byte offset addresses */
-	{
-	
+
+	int total_clocks = 0;
+	for (haltflag=0; haltflag; total_clocks++)	/* PC COUNT STAGE */
+	{	
 		printf("@PC=0x%08X, Opcode=0x%02X, ", i, opcode = mem[i/4] >> 26);
-	if (opcode == 0) {
-		printf("\nFunc: %i, %x\n", mem[i/4] & 0x3F, mem[i/4] & 0x3F);
-		printf("R Type, Function=0x%02X (%s)\n", mem[i/4] & 0x3F, ftyp[mem[i/4] & 0x3F]);
-		rs = (mem[i/4] >> 21) & 15;
-		rt = (mem[i/4] >> 16) & 15;
-		rd = (mem[i/4] >> 11) & 15;
-		shamt = (mem[i/4] >> 6) & 15;
-		printf(" Rs=%i, (%s), Rt=%i (%s), Rd=%i, (%s), shamt=%i\n\n", rs, REG[rs], rt, REG[rt], rd, REG[rd], shamt);
-	}
-	else if (opcode >= 8 && opcode <= 14) {
-		printf("I Type (%s)\n", optyp[opcode]);
-		rs = (mem[i/4] >> 21) & 15;
-		rt = (mem[i/4] >> 16) & 15;
-		imm = mem[i/4] & 0xFFFF;
-		if (imm & 0x8000)
-			signext = imm + 0xFFFF0000;
-		else
-			signext = imm;
-		printf(" Rs=%i, (%s), Rt=%i (%s), Imm=0x%04X, signext: 0x%08X (%i)\n\n", rs, REG[rs], rt, REG[rt], imm, signext, signext);
-	}
-	else if (opcode >= 4 && opcode <= 5) {
-		printf("I Type (%s)\n", optyp[opcode]);
-		rs = (mem[i/4] >> 21) & 15;
-		rt = (mem[i/4] >> 16) & 15;
-		imm = mem[i/4] & 0xFFFF;
-		if (imm & 0x8000)
-			signext = ((imm ^ 0xFFFF)+1)*-1;
-		else
-			signext = imm;
-		printf(" Rs=%i, (%s), Rt=%i (%s), Imm=0x%04X, signext: 0x%08X (%i),\n", rs, REG[rs], rt, REG[rt], imm, signext, signext);
-		printf("  BranchAddr=0x%08X\n\n", imm*4);
-	}
-	else if (opcode >= 16 && opcode <= 43) {
-		printf("I Type (%s)\n", optyp[opcode]);
-		rs = (mem[i/4] >> 21) & 15;
-		rt = (mem[i/4] >> 16) & 15;
-		imm = mem[i/4] & 0xFFFF;
-		if (imm & 0x8000)
-			signext = ((imm ^ 0xFFFF)+1)*-1;
-		else
-			signext = imm;
-		printf(" Rs=%i, (%s), Rt=%i (%s), Imm=0x%04X, signext: 0x%08X (%i),\n", rs, REG[rs], rt, REG[rt], imm, signext, signext);
-		printf(" EffAddr=R[%s] + 0x%08X\n\n", REG[rs], signext);
-	}
-	else if (opcode >= 2 && opcode <= 3) {
-		printf("J Type (%s), addr=0x%06X\n", optyp[opcode], mem[i/4] & 0x3FFFFFF);
+		PCCOUNT = i;
+		if (opcode == 0) {
+			printf("\nFunc: %i, %x\n", mem[i/4] & 0x3F, mem[i/4] & 0x3F);
+			printf("R Type, Function=0x%02X (%s)\n", mem[i/4] & 0x3F, ftyp[mem[i/4] & 0x3F]);
+			rs = (mem[i/4] >> 21) & 15;
+			rt = (mem[i/4] >> 16) & 15;
+			rd = (mem[i/4] >> 11) & 15;
+			shamt = (mem[i/4] >> 6) & 15;
+			printf(" Rs=%i, (%s), Rt=%i (%s), Rd=%i, (%s), shamt=%i\n\n", rs, REG[rs], rt, REG[rt], rd, REG[rd], shamt);
+		}
+		else if (opcode >= 8 && opcode <= 14) {
+			printf("I Type (%s)\n", optyp[opcode]);
+		oad code, initialize structures, etc…	rs = (mem[i/4] >> 21) & 15;
+			rt = (mem[i/4] >> 16) & 15;
+			imm = mem[i/4] & 0xFFFF;
+			if (imm & 0x8000)
+				signext = imm + 0xFFFF0000;
+			else
+				signext = imm;
+			printf(" Rs=%i, (%s), Rt=%i (%s), Imm=0x%04X, signext: 0x%08X (%i)\n\n", rs, REG[rs], rt, REG[rt], imm, signext, signext);
+		}
+		else if (opcode >= 4 && opcode <= 5) {
+			printf("I Type (%s)\n", optyp[opcode]);
+			rs = (mem[i/4] >> 21) & 15;
+			rt = (mem[i/4] >> 16) & 15;
+			imm = mem[i/4] & 0xFFFF;
+			if (imm & 0x8000)
+				signext = ((imm ^ 0xFFFF)+1)*-1;
+			else
+				signext = imm;
+			printf(" Rs=%i, (%s), Rt=%i (%s), Imm=0x%04X, signext: 0x%08X (%i),\n", rs, REG[rs], rt, REG[rt], imm, signext, signext);
+			printf("  BranchAddr=0x%08X\n\n", imm*4);
+		}
+		else if (opcode >= 16 && opcode <= 43) {
+			printf("I Type (%s)\n", optyp[opcode]);
+			rs = (mem[i/4] >> 21) & 15;
+			rt = (mem[i/4] >> 16) & 15;
+			imm = mem[i/4] & 0xFFFF;
+			if (imm & 0x8000)
+				signext = ((imm ^ 0xFFFF)+1)*-1;
+			else
+				signext = imm;
+			printf(" Rs=%i, (%s), Rt=%i (%s), Imm=0x%04X, signext: 0x%08X (%i),\n", rs, REG[rs], rt, REG[rt], imm, signext, signext);
+ad code, initialize structures, etc…
+			printf(" EffAddr=R[%s] + 0x%08X\n\n", REG[rs], signext);
+		}
+		else if (opcode >= 2 && opcode <= 3) {
+			printf("J Type (%s), addr=0x%06X\n", optyp[opcode], mem[i/4] & 0x3FFFFFF);
 		
-		printf(" JumpAddr=0x%08X\n\n", (mem[i/4] & 0x3FFFFFF)*4);
-	} 
+			printf(" JumpAddr=0x%08X\n\n", (mem[i/4] & 0x3FFFFFF)*4);
+		}
+		id(REGFILE);
+		if(i);
 	}
+	
 	exit(0);
 }
+ad code, initialize structures, etc…oad code, initialize structures, etc…
